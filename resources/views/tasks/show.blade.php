aca906b24d038395feb0cdceb155114878bf281e
Jim Baldwin <jim@jimbaldwin.net>
2016-06-11 10:52:17 -0700

    Add authentication

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,45 @@
+@extends('app')
+
+@section('content')
+    <h2>{{ $title }}</h2>
+    <h4>Tasks</h4>
+
+
+    <div></div>
+
+    <table class="table table-bordered" id="projects-table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Slug</th>
+        </tr>
+        </thead>
+    </table>
+
+    <p>
+        create project
+    </p>
+@endsection
+
+@push('scripts')
+<script>
+    $(function() {
+
+        $('#projects-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: '/datatables/data2/{{ $project_id }}',
+            columns: [
+                { data: 'id', name: 'id' },
+                { data: 'name', name: 'name' },
+                { data: 'slug', name: 'slug' }
+
+            ]
+        });
+    });
+</script>
+@endpush
+
+
+
